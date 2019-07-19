9e3562406d373a8f53888ec418fdea6e2ccd8773
taka-sho <takasho@keio.jp>
2019-07-20 01:39:32 +0900

    chore(sidebar) display menu items correctly

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -20,84 +20,105 @@ const sidebar: React.SFC<any> = ({
     // defaultCollapsed
     theme='light'
   >
-    <Menu
-      mode='inline'
-      theme='light'
-      defaultSelectedKeys={['1']}
-      defaultOpenKeys={['sub1']}
-    >
-      {/* <Item key='1'>
-        <Icon type='home' />
-        <span>ホーム</span>
-        <Link to='/' />
-      </Item> */}
-      <Item key='2' disabled={position!=='writer'}>
-        <Icon type='file-search' />
-        <span>募集中</span>
-        <Link to='/articles/recruiting' />
-      </Item>
-      <Item key='3' disabled={position!=='writer'}>
-        <Badge dot={writings} offset={[ -8, 0 ]} >
-          <Icon type='edit' />
-        </Badge>
-        <span>受注中</span>
-        <Link to='/articles/ordered' />
-      </Item>
-      <Item key='4' disabled={position!=='writer'}>
-        <Badge dot={pendings} offset={[ -8, 0 ]} >
-          <Icon type='clock-circle' />
-        </Badge>
-        <span>検品中</span>
-        <Link to='/articles/pending' />
-      </Item>
-      <Item key='5' disabled={position!=='writer'}>
-        <Badge dot={rejects} offset={[ -8, 0 ]} >
-          <Icon type='stop' />
-        </Badge>
-        <span>差し戻し</span>
-        <Link to='/articles/rejected' />
-      </Item>
-      <Item key='6' disabled={position!=='writer'}>
-        <Icon type='check' />
-        <span>納品済み</span>
-        <Link to='/articles/accepted' />
-      </Item>
-      <Item key='7' disabled={position!=='director'}>
-        <Icon type='file-add' />
-        <span>発注</span>
-        <Link to='/order' />
-      </Item>
-      <Item key='8' disabled={position!=='director'}>
-        <Icon type='eye' />
-        <span>検品</span>
-        <Link to='/checkList' />
-      </Item>
-      {/* <Item key='5'>
-        <Icon type='user' />
-        <span>マイページ</span>
-        <Link to='/mypage' />
-      </Item> */}
-      <Item key='9' disabled={position!=='director'}>
-        <Icon type='tags' />
-        <span>カテゴリー・タグ</span>
-        <Link to='/categoriesAndTags' />
-      </Item>
-      <Item key='10' disabled={position!==''}>
-        <Icon type='login' />
-        <span>ログイン</span>
-        <Link to='/login' />
-      </Item>
-      <Item key='11' disabled={!isAuth}>
-        <Icon type='logout' />
-        <span>ログアウト</span>
-        <Link to='/logout' />
-      </Item>
-      <Item key='12' disabled={position!==''}>
-        <Icon type='user-add' />
-        <span>新規登録</span>
-        <Link to='/signup' />
-      </Item>
-    </Menu>
+    {
+      position === 'writer' ? (
+        <Menu
+          mode='inline'
+          theme='light'
+          defaultSelectedKeys={['1']}
+          defaultOpenKeys={['sub1']}
+        >
+          <Item key='2' disabled={position!=='writer'}>
+            <Icon type='file-search' />
+            <span>募集中</span>
+            <Link to='/articles/recruiting' />
+          </Item>
+          <Item key='3' disabled={position!=='writer'}>
+            <Badge dot={writings} offset={[ -8, 0 ]} >
+              <Icon type='edit' />
+            </Badge>
+            <span>受注中</span>
+            <Link to='/articles/ordered' />
+          </Item>
+          <Item key='4' disabled={position!=='writer'}>
+            <Badge dot={pendings} offset={[ -8, 0 ]} >
+              <Icon type='clock-circle' />
+            </Badge>
+            <span>検品中</span>
+            <Link to='/articles/pending' />
+          </Item>
+          <Item key='5' disabled={position!=='writer'}>
+            <Badge dot={rejects} offset={[ -8, 0 ]} >
+              <Icon type='stop' />
+            </Badge>
+            <span>差し戻し</span>
+            <Link to='/articles/rejected' />
+          </Item>
+          <Item key='6' disabled={position!=='writer'}>
+            <Icon type='check' />
+            <span>納品済み</span>
+            <Link to='/articles/accepted' />
+          </Item>
+          <Item key='11' disabled={!isAuth}>
+            <Icon type='logout' />
+            <span>ログアウト</span>
+            <Link to='/logout' />
+          </Item>
+        </Menu>
+      ) : null
+    }
+    {
+      position === 'director' ? (
+        <Menu
+          mode='inline'
+          theme='light'
+          defaultSelectedKeys={['1']}
+          defaultOpenKeys={['sub1']}
+        >
+          <Item key='7' disabled={position!=='director'}>
+            <Icon type='file-add' />
+            <span>発注</span>
+            <Link to='/order' />
+          </Item>
+          <Item key='8' disabled={position!=='director'}>
+            <Icon type='eye' />
+            <span>検品</span>
+            <Link to='/checkList' />
+          </Item>
+          <Item key='9' disabled={position!=='director'}>
+            <Icon type='tags' />
+            <span>カテゴリー・タグ</span>
+            <Link to='/categoriesAndTags' />
+          </Item>
+          <Item key='11' disabled={!isAuth}>
+            <Icon type='logout' />
+            <span>ログアウト</span>
+            <Link to='/logout' />
+          </Item>
+        </Menu>
+      ) : null
+    }
+    {
+      position === '' ? (
+        <Menu
+          mode='inline'
+          theme='light'
+          defaultSelectedKeys={['1']}
+          defaultOpenKeys={['sub1']}
+        >
+          <Item key='10' disabled={position!==''}>
+            <Icon type='login' />
+            <span>ログイン</span>
+            <Link to='/login' />
+          </Item>
+          <Item key='12' disabled={position!==''}>
+            <Icon type='user-add' />
+            <span>新規登録</span>
+            <Link to='/signup' />
+          </Item>
+        </Menu>
+      ) : null
+    }
   </Sider>
 )
 
